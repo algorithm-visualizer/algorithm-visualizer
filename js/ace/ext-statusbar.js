f2ddec834cab26ef700de9e3e8e434148e214e49
Jason Park <parkjs814@gmail.com>
2016-05-14 19:33:52 -0500

    first commit

---
+++ js/ace/ext-statusbar.js
@@ -0,0 +1,5 @@
+ace.define("ace/ext/statusbar",["require","exports","module","ace/lib/dom","ace/lib/lang"],function(e,t,n){"use strict";var r=e("ace/lib/dom"),i=e("ace/lib/lang"),s=function(e,t){this.element=r.createElement("div"),this.element.className="ace_status-indicator",this.element.style.cssText="display: inline-block;",t.appendChild(this.element);var n=i.delayedCall(function(){this.updateStatus(e)}.bind(this)).schedule.bind(null,100);e.on("changeStatus",n),e.on("changeSelection",n),e.on("keyboardActivity",n)};(function(){this.updateStatus=function(e){function n(e,n){e&&t.push(e,n||"|")}var t=[];n(e.keyBinding.getStatusText(e)),e.commands.recording&&n("REC");var r=e.selection,i=r.lead;if(!r.isEmpty()){var s=e.getSelectionRange();n("("+(s.end.row-s.start.row)+":"+(s.end.column-s.start.column)+")"," ")}n(i.row+":"+i.column," "),r.rangeCount&&n("["+r.rangeCount+"]"," "),t.pop(),this.element.textContent=t.join("")}}).call(s.prototype),t.StatusBar=s});
+                (function() {
+                    ace.require(["ace/ext/statusbar"], function() {});
+                })();
+            
---
+++ js/tracer.js
@@ -0,0 +1,109 @@
+var timer = null;
+
+var Tracer = function (module) {
+    this.module = module;
+    this.traces = [];
+    this.pause = false;
+    this.traceOptions = null;
+    this.traceIndex = -1;
+};
+
+Tracer.prototype.resize = function () {
+};
+
+Tracer.prototype.reset = function () {
+    this.traces = [];
+    if (timer) clearTimeout(timer);
+    $('#tab_trace .wrapper').empty();
+};
+
+Tracer.prototype.setData = function () {
+};
+
+Tracer.prototype.pushStep = function (step, delay) {
+    var len = this.traces.length;
+    var last = [];
+    if (len == 0) {
+        this.traces.push(last);
+    } else {
+        last = this.traces[len - 1];
+    }
+    last.push(step);
+    if (delay) this.traces.push([]);
+};
+
+Tracer.prototype.print = function (msg, delay) {
+    this.pushStep({type: 'print', msg: msg}, delay);
+};
+
+Tracer.prototype.visualize = function (options) {
+    options = options || {};
+    options.interval = options.interval || 500;
+
+    $('#btn_trace').click();
+    this.traceOptions = options;
+    this.traceIndex = -1;
+    this.resumeStep();
+};
+
+Tracer.prototype.isPause = function () {
+    return this.pause;
+};
+
+Tracer.prototype.pauseStep = function () {
+    if (this.traceIndex < 0) return;
+    this.pause = true;
+    if (timer) clearTimeout(timer);
+    $('#btn_pause').addClass('active');
+};
+
+Tracer.prototype.resumeStep = function () {
+    this.pause = false;
+    this.step(this.traceIndex + 1);
+    $('#btn_pause').removeClass('active');
+};
+
+Tracer.prototype.step = function (i, options) {
+    if (isNaN(i) || i >= this.traces.length || i < 0) return;
+    options = options || {};
+
+    this.traceIndex = i;
+    var trace = this.traces[i];
+    var tracer = this;
+    trace.forEach(function (step, i) {
+        switch (step.type) {
+            case 'print':
+                printTrace(step.msg);
+                break;
+            default:
+                tracer.module.prototype.processStep.call(tracer, step, options);
+        }
+    });
+    if (!options.virtual) {
+        this.refresh();
+        scrollToEnd();
+    }
+    if (this.pause) return;
+    timer = setTimeout(function () {
+        tracer.step(i + 1, options);
+    }, this.traceOptions.interval);
+};
+
+Tracer.prototype.refresh = function () {
+};
+
+Tracer.prototype.prevStep = function () {
+    this.step(this.traceIndex - 1);
+};
+
+Tracer.prototype.nextStep = function () {
+    this.step(this.traceIndex + 1);
+};
+
+var printTrace = function (message) {
+    $('#tab_trace .wrapper').append($('<span>').append(message + '<br/>'));
+};
+
+var scrollToEnd = function () {
+    $('#tab_trace').animate({scrollTop: $('#tab_trace')[0].scrollHeight}, 100);
+};
