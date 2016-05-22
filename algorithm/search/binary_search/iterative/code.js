9d6a32690e3480a0d30c56b5a33a745373290030
Jason Park <parkjs814@gmail.com>
2016-05-22 04:18:04 -0500

    change binary_search to find random element

--- algorithm/search/binary_search/iterative/code.js
+++ algorithm/search/binary_search/iterative/code.js
@@ -1,42 +1,42 @@
 function BinarySearch(array, element) { // array = sorted array, element = element to be found,
 
-  var minIndex = 0;
-  var maxIndex = array.length - 1;
-  var currentIndex;
-  var testElement;
+    var minIndex = 0;
+    var maxIndex = array.length - 1;
+    var currentIndex;
+    var testElement;
 
-  while (minIndex <= maxIndex) {
+    while (minIndex <= maxIndex) {
 
-    middleIndex = Math.floor((minIndex + maxIndex) / 2);
-    testElement = array[middleIndex];
+        middleIndex = Math.floor((minIndex + maxIndex) / 2);
+        testElement = array[middleIndex];
 
-    tracer._print('Searching at index: ' + middleIndex);
-    tracer._notify(middleIndex);
+        tracer._print('Searching at index: ' + middleIndex);
+        tracer._notify(middleIndex);
 
-    if (testElement < element) {
+        if (testElement < element) {
 
-      tracer._print('Going right.');
-      minIndex = middleIndex + 1;
+            tracer._print('Going right.');
+            minIndex = middleIndex + 1;
 
-    } else if (testElement > element) {
+        } else if (testElement > element) {
 
-      tracer._print('Going left.');
-      maxIndex = middleIndex - 1;
+            tracer._print('Going left.');
+            maxIndex = middleIndex - 1;
 
-    } else {
+        } else {
 
-      tracer._print(element + ' is found at position ' + middleIndex + '!');
-      tracer._select(middleIndex);
+            tracer._print(element + ' is found at position ' + middleIndex + '!');
+            tracer._select(middleIndex);
 
-      return middleIndex;
+            return middleIndex;
+        }
     }
-  }
 
-  tracer._print(element + ' is not found!');
-  return -1;
+    tracer._print(element + ' is not found!');
+    return -1;
 }
 
-var element = D[0];
+var element = D[Math.random() * D.length | 0];
 
 tracer._sleep(1000);
 tracer._pace(1000);
--- algorithm/search/binary_search/recursive/code.js
+++ algorithm/search/binary_search/recursive/code.js
@@ -1,36 +1,36 @@
 function BinarySearch(array, element, minIndex, maxIndex) { // array = sorted array, element = element to be found, minIndex = minIndex index, maxIndex = maxIndex index
-  if (minIndex > maxIndex) {
+    if (minIndex > maxIndex) {
+        tracer._print(element + ' is not found!');
+        return -1;
+    }
+
+    var middleIndex = Math.floor((minIndex + maxIndex) / 2);
+    var testElement = array[middleIndex];
+
+    tracer._print('Searching at index: ' + middleIndex);
+    tracer._notify(middleIndex);
+
+    if (testElement < element) {
+        tracer._print('Going right.');
+        return BinarySearch(array, element, middleIndex + 1, maxIndex);
+    }
+
+    if (testElement > element) {
+        tracer._print('Going left.');
+        return BinarySearch(array, element, minIndex, middleIndex - 1);
+    }
+
+    if (testElement === element) {
+        tracer._print(element + ' is found at position ' + middleIndex + '!');
+        tracer._select(middleIndex);
+        return middleIndex;
+    }
+
     tracer._print(element + ' is not found!');
     return -1;
-  }
-
-  var middleIndex = Math.floor((minIndex + maxIndex) / 2);
-  var testElement = array[middleIndex];
-
-  tracer._print('Searching at index: ' + middleIndex);
-  tracer._notify(middleIndex);
-
-  if (testElement < element) {
-    tracer._print('Going right.');
-    return BinarySearch(array, element, middleIndex + 1, maxIndex);
-  }
-
-  if (testElement > element) {
-    tracer._print('Going left.');
-    return BinarySearch(array, element, minIndex, middleIndex - 1);
-  }
-
-  if (testElement === element) {
-    tracer._print(element + ' is found at position ' + middleIndex + '!');
-    tracer._select(middleIndex);
-    return middleIndex;
-  }
-
-  tracer._print(element + ' is not found!');
-  return -1;
 }
 
-var element = D[0];
+var element = D[Math.random() * D.length | 0];
 
 tracer._sleep(1000);
 tracer._pace(1000);
