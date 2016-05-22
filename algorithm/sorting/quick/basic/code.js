eafc8e98d29bd60db63ce1d3ffca5ff28bd8412f
Philip YS <wbvcos@gmail.com>
2016-05-23 07:05:11 +0900

    Fix define warning in "temp" variable

--- algorithm/sorting/quick/basic/code.js
+++ algorithm/sorting/quick/basic/code.js
@@ -1,6 +1,7 @@
 tracer._print('original array = [' + D.join(', ') + ']');
 tracer._sleep(1000);
 tracer._pace(500);
+
 function quicksort(low, high) {
     if (low < high) {
         var p = partition(low, high);
@@ -8,25 +9,29 @@ function quicksort(low, high) {
         quicksort(p + 1, high);
     }
 }
+
 function partition(low, high) {
     var pivot = D[high];
     tracer._selectSet([low, high]);
     var i = low;
+    var temp;
+    
     for (var j = low; j < high; j++) {
         if (D[j] <= pivot) {
-            var temp = D[i];
+            temp = D[i];
             D[i] = D[j];
             D[j] = temp;
             tracer._notify(i, j);
             i++;
         }
     }
-    var temp = D[i];
+    temp = D[i];
     D[i] = D[high];
     D[high] = temp;
     tracer._notify(i, high);
     tracer._deselectSet([low, high]);
     return i;
 }
+
 quicksort(0, D.length - 1);
 tracer._print('sorted array = [' + D.join(', ') + ']');
