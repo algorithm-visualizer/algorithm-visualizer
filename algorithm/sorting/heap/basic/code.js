4d50aaefdb0c230c1b48aeab6c877a19de258c61
thinker3197 <ashishgupta.3197@gmail.com>
2016-05-22 16:16:25 +0530

    Add code and data

---
+++ algorithm/sorting/heap/basic/code.js
@@ -0,0 +1,57 @@
+tracer._print('Original array = [' + D.join(', ') + ']');
+
+function heapSort(array, size) {
+    var i, j, temp;
+
+    for (i = Math.ceil(size / 2) - 1; i >= 0; i--) {
+        heapify(array, size, i);
+    }
+
+    for (j = size - 1; j >= 0; j--) {
+        temp = array[0];
+        array[0] = array[j];
+        array[j] = temp;
+
+        tracer._select(j);
+
+        heapify(array, j, 0);
+        tracer._print('Swapping elemnts : ' + array[0] + ' & ' + array[j]);
+
+        tracer._deselect(j);
+    }
+}
+
+function heapify(array, size, root) {
+
+    var largest = root;
+    var left = 2 * root + 1;
+    var right = 2 * root + 2;
+    var temp;
+
+    if (left < size && array[left] > array[largest]) {
+        largest = left;
+    }
+
+    if (right < size && array[right] > array[largest]) {
+        largest = right;
+    }
+
+    if (largest != root) {
+        temp = array[root];
+        array[root] = array[largest];
+        array[largest] = temp;
+
+        tracer._notify(largest, root);
+
+        tracer._print('Swapping elemnts : ' + array[root] + ' & ' + array[largest]);
+
+        heapify(array, size, largest);
+    }
+}
+
+tracer._sleep(1000);
+tracer._pace(800);
+
+heapSort(D, 10);
+
+tracer._print('Final array = [' + D.join(', ') + ']');
