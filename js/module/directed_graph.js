f6474a08fc91bfa46244c048dda891672d84d440
Jason Park <parkjs814@gmail.com>
2016-05-23 00:29:49 -0500

    add WeightedUndirectedGraph

--- js/module/directed_graph.js
+++ js/module/directed_graph.js
@@ -335,7 +335,9 @@ var DirectedGraph = {
         for (var i = 0; i < N; i++) {
             G[i] = new Array(N);
             for (var j = 0; j < N; j++) {
-                if (i != j) G[i][j] = (Math.random() * (1 / ratio) | 0) == 0 ? 1 : 0;
+                if (i != j) {
+                    G[i][j] = (Math.random() * (1 / ratio) | 0) == 0 ? 1 : 0;
+                }
             }
         }
         return G;
