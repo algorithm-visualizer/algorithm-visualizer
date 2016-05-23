22aa941a219f66d26bed4b2cf474efbad83c3a4f
Jason Park <parkjs814@gmail.com>
2016-05-22 22:17:52 -0500

    change a bit of bellman_ford

--- algorithm/graph_search/bellman_ford/shortest_path/data.js
+++ algorithm/graph_search/bellman_ford/shortest_path/data.js
@@ -1,3 +1,9 @@
 var tracer = new WeightedDirectedGraphTracer();
-var G = WeightedDirectedGraph.random(5, .5, -5, 5);
+var G = [
+    [0, -1, 4, 0, 0],
+    [0, 0, 3, 2, 2],
+    [0, 0, 0, 0, 0],
+    [0, 1, 5, 0, 0],
+    [0, 0, 0, -3, 0]
+];
 tracer._setData(G);
