514f903d92886228c73ab12843083bc339305f39
duaraghav8@gmail <duaraghav8@gmail.com>
2016-05-25 11:06:17 +0530

    Fixed tracer._print() statements for max_subarray

--- algorithm/etc/dp/max_subarray/code.js
+++ algorithm/etc/dp/max_subarray/code.js
@@ -1,22 +1,22 @@
 var maxSubarraySum = (function maxSubarray (array) {
-	var maxSoFar = 0,
+	var maxSoFar = 0,print
 		maxEndingHere = 0;
 
-	tracer._print ('Initializing maxSoFar = 0 & maxEndingHere = 0');
+	logger._print ('Initializing maxSoFar = 0 & maxEndingHere = 0');
 
 	for (var i = 0; i < array.length; i++) {
 		tracer._select (i);
-		tracer._print (maxEndingHere + ' + ' + array [i]);
+		logger._print (maxEndingHere + ' + ' + array [i]);
 		maxEndingHere += array [i];
-		tracer._print ('=> ' + maxEndingHere);
+		logger._print ('=> ' + maxEndingHere);
 
 		if (maxEndingHere < 0) {
-			tracer._print ('maxEndingHere is negative, set to 0');
+			logger._print ('maxEndingHere is negative, set to 0');
 			maxEndingHere = 0;
 		}
 
 		if (maxSoFar < maxEndingHere) {
-			tracer._print ('maxSoFar < maxEndingHere, setting maxSoFar to maxEndingHere (' + maxEndingHere + ')');
+			logger._print ('maxSoFar < maxEndingHere, setting maxSoFar to maxEndingHere (' + maxEndingHere + ')');
 			maxSoFar = maxEndingHere;
 		}
 		
@@ -26,4 +26,4 @@ var maxSubarraySum = (function maxSubarray (array) {
 	return maxSoFar;
 }) (D);
 
-tracer._print ('Maximum Subarray\'s Sum is: ' + maxSubarraySum);
+logger._print ('Maximum Subarray\'s Sum is: ' + maxSubarraySum);
