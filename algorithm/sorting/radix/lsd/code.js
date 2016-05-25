ded96c34907870fd0093c132a14f3459b84f6840
Jason Park <parkjs814@gmail.com>
2016-05-25 00:29:36 -0500

    fix algorithms

--- algorithm/sorting/radix/lsd/code.js
+++ algorithm/sorting/radix/lsd/code.js
@@ -1,44 +1,54 @@
-tracer._print('original array = [' + D[0].join(', ') + ']');
-tracer._sleep(1000);
-tracer._pace(300);
-function pow(base, expo){
-	var ans = 1;
-	for(var i = 0; i < expo;i++){
-		ans *= base;
-	}
-	return ans;
+logger._print('original array = [' + D[0].join(', ') + ']');
+function pow(base, expo) {
+    var ans = 1;
+    for (var i = 0; i < expo; i++) {
+        ans *= base;
+    }
+    return ans;
 }
-for(var exp = 0; exp < 3;exp ++){
-	tracer._print("Digit: "+exp);
-	for(var i = 0; i < D[0].length; i++){
-		tracer._select(0, i);
-		D[2][ parseInt( D[0][i] / pow(10, exp)  % 10) ] += 1;
-		tracer._notify(2, parseInt( D[0][i] / pow(10, exp)  % 10) );
-		tracer._deselect(0, i);
-	}
-	for(var i = 1; i < 10; i++){
-		tracer._select(2, i - 1);
-		D[2][i] += D[2][i - 1];
-		tracer._notify(2, i);
-		tracer._deselect(2, i - 1);
-	}
-	for(var i = D[0].length - 1; i >= 0; i--){
-		tracer._select(0, i);
-		D[2][parseInt( D[0][i] / pow(10, exp)  % 10) ] -= 1;
-		tracer._notify(2, parseInt( D[0][i] / pow(10, exp)  % 10) );
-		D[1][ D[2][ parseInt( D[0][i] / pow(10, exp)  % 10) ] ] = D[0][i];
-		tracer._notify(1, D[2][ parseInt( D[0][i] / pow(10, exp)  % 10) ] );
-		tracer._deselect(0, i);
-	}
-	for(var i = 0; i < D[0].length; i++){
-		tracer._select(1, i);
-		D[0][i] = D[1][i];
-		tracer._notify(0, i);
-		tracer._deselect(1, i);
-	}
-	for(var i = 0; i < 10; i++){
-		D[2][i] = 0;
-		tracer._notify(2, i);
-	}	
+function digit(i, exp) {
+    return parseInt(D[0][i] / pow(10, exp) % 10);
 }
-tracer._print('sorted array = [' + D[0].join(', ') + ']');
+for (var exp = 0; exp < 3; exp++) {
+    logger._print("Digit: " + exp);
+    var i;
+    for (i = 0; i < D[0].length; i++) {
+        var d = digit(i, exp);
+        tracer._select(0, i)._wait();
+        D[2][d] += 1;
+        tracer._notify(2, d, D[2][d])._wait();
+        tracer._denotify(2, d);
+        tracer._deselect(0, i);
+    }
+    for (i = 1; i < 10; i++) {
+        tracer._select(2, i - 1)._wait();
+        D[2][i] += D[2][i - 1];
+        tracer._notify(2, i, D[2][i])._wait();
+        tracer._denotify(2, i);
+        tracer._deselect(2, i - 1);
+    }
+    for (i = D[0].length - 1; i >= 0; i--) {
+        var d = digit(i, exp);
+        tracer._select(0, i)._wait();
+        D[2][d] -= 1;
+        tracer._notify(2, d, D[2][d])._wait();
+        tracer._denotify(2, d);
+        D[1][D[2][d]] = D[0][i];
+        tracer._notify(1, D[2][d], D[1][D[2][d]])._wait();
+        tracer._denotify(1, D[2][d]);
+        tracer._deselect(0, i);
+    }
+    for (i = 0; i < D[0].length; i++) {
+        tracer._select(1, i)._wait();
+        D[0][i] = D[1][i];
+        tracer._notify(0, i, D[0][i])._wait();
+        tracer._denotify(0, i);
+        tracer._deselect(1, i);
+    }
+    for (i = 0; i < 10; i++) {
+        D[2][i] = 0;
+        tracer._notify(2, i, D[2][i])._wait();
+        tracer._denotify(2, i);
+    }
+}
+logger._print('sorted array = [' + D[0].join(', ') + ']');
