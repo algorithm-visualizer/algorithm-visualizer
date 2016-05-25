41597bfb86a7e668400451abb8d6a1a25dcf9122
ma4413 <michael.akintunde13@imperial.ac.uk>
2016-05-26 00:45:31 +0100

    Fix tracer problems

--- algorithm/sorting/shell/basic/code.js
+++ algorithm/sorting/shell/basic/code.js
@@ -7,15 +7,21 @@ for (var gap = N; gap = parseInt(gap / 2);) {
     for (var i = gap; i < N; i++) {
         tracer._select(i)._select(i - gap)._wait();
         var k = D[i];
+        logger._print('Holding: ' + k)
         for (var j = i; j >= gap && k < D[j - gap]; j -= gap) {
-            logger._print('Swap ' + D[j] + ' and ' + D[j - gap]);
-            tracer._notify(j, D[j])._notify(j - gap, D[j - gap])._wait();
+            logger._print(k + ' < ' + D[j - gap]);
             D[j] = D[j - gap];
-            tracer._denotify(i - 1)._denotify(i);
+            tracer._notify(j, D[j])._wait();
+            tracer._denotify(j);
         }
+        var old = D[j];
         D[j] = k;
-        tracer._notify(j,D[j]);
-        tracer._denotify(j);
+        if (old != k) {
+            tracer._notify(j,D[j])._wait();
+            tracer._denotify(j);
+            logger._print('Swapped ' + D[j] + ' with ' + old);
+        }
+
         tracer._deselect(i)._deselect(i - gap);
     }
 }
