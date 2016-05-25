7a9310728eea05bfac34363145970c738e5b1a12
Jason Park <parkjs814@gmail.com>
2016-05-25 02:00:49 -0500

    fix kruskal

--- algorithm/mst/kruskal/normal/code.js
+++ algorithm/mst/kruskal/normal/code.js
@@ -1,50 +1,51 @@
-function kruskal () {
-  var vcount = G.length;
+function kruskal() {
+    var vcount = G.length;
 
-  // Preprocess: sort edges by weight.
-  var edges = [];
-  for (var vi = 0; vi < vcount - 1; vi++) {
-    for (var vj = vi + 1; vj < vcount; vj++) {
-      edges.push({
-        0: vi,
-        1: vj,
-        weight: G[vi][vj]
-      });
+    // Preprocess: sort edges by weight.
+    var edges = [];
+    for (var vi = 0; vi < vcount - 1; vi++) {
+        for (var vj = vi + 1; vj < vcount; vj++) {
+            edges.push({
+                0: vi,
+                1: vj,
+                weight: G[vi][vj]
+            });
+        }
     }
-  }
-  edges.sort(function (ei, ej) { return ei.weight - ej.weight });
+    edges.sort(function (ei, ej) {
+        return ei.weight - ej.weight
+    });
 
-  // Give each vertex a tree to decide if they are already in the same tree.
-  var t = [];
-  for (var i = 0; i < vcount; i++) {
-    t[i] = {};
-    t[i][i] = true;
-  }
-
-  var wsum = 0;
-  for (var n = 0; n < vcount - 1 && edges.length > 0;) {
-    var e = edges.shift(); // Get the edge of min weight
-    tracer._visit(e[0], e[1]);
-    if (t[e[0]] === t[e[1]]) {
-      // e[0] & e[1] already in the same tree, ignore
-      tracer._leave(e[0], e[1]);
-      continue;
+    // Give each vertex a tree to decide if they are already in the same tree.
+    var t = [];
+    for (var i = 0; i < vcount; i++) {
+        t[i] = {};
+        t[i][i] = true;
     }
 
-    // Choose the current edge.
-    wsum += e.weight;
+    var wsum = 0;
+    for (var n = 0; n < vcount - 1 && edges.length > 0;) {
+        var e = edges.shift(); // Get the edge of min weight
+        tracer._visit(e[0], e[1])._wait();
+        if (t[e[0]] === t[e[1]]) {
+            // e[0] & e[1] already in the same tree, ignore
+            tracer._leave(e[0], e[1])._wait();
+            continue;
+        }
 
-    // Merge tree of e[0] & e[1]
-    var tmerged = {};
-    for (i in t[e[0]]) tmerged[i] = true;
-    for (i in t[e[1]]) tmerged[i] = true;
-    for (i in tmerged) t[i] = tmerged;
+        // Choose the current edge.
+        wsum += e.weight;
 
-    n += 1;
-  }
+        // Merge tree of e[0] & e[1]
+        var tmerged = {};
+        for (i in t[e[0]]) tmerged[i] = true;
+        for (i in t[e[1]]) tmerged[i] = true;
+        for (i in tmerged) t[i] = tmerged;
 
-  tracer._print("The sum of all edges is: " + wsum);
+        n += 1;
+    }
+
+    logger._print("The sum of all edges is: " + wsum);
 }
 
-tracer._pace(500);
 kruskal();
