7ce51b33a5be575a54d9331e1a96ae2ba865422a
TornjV <tornjanski.veljko@gmail.com>
2016-05-26 09:52:22 +0200

    Remove leftover dp files

--- algorithm/etc/dp/fibonacci/data.js
+++
@@ -1,7 +0,0 @@
-var tracer = new Array1DTracer();
-var index = 15;
-var D = [1, 1];
-for (var i = 2; i < index; i++) {
-    D.push(0);
-}
-tracer._setData(D);
--- algorithm/etc/dp/longest_increasing_subsequence/data.js
+++
@@ -1,5 +0,0 @@
-var tracer = new Array1DTracer();
-var logger = new LogTracer();
-var A = Array1D.random(10, 0, 10);
-var LIS = new Array(A.length);
-tracer._setData(A);
