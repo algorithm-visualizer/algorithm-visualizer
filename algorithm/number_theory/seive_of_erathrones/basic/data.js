bcb0183829509547204936f7e9f573e4a44bcaaa
Jason Park <parkjs814@gmail.com>
2016-06-02 17:12:32 +0900

    minor change in seive of erathrones

--- algorithm/number_theory/seive_of_erathrones/basic/data.js
+++ algorithm/number_theory/seive_of_erathrones/basic/data.js
@@ -1,10 +1,10 @@
 var tracer = new Array1DTracer('Seive');
-length = 30;
+var N = 30;
 var a = [];
 var b = [];
-for (var i=1;i<=length;i++) {
-	a.push(i);
-	b.push(0);
+for (var i = 1; i <= N; i++) {
+  a.push(i);
+  b.push(0);
 }
 tracer._setData(a);
 var logger = new LogTracer();
