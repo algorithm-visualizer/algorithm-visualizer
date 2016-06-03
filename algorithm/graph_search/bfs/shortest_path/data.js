3df049b2391c0510f74e0897228d840fb0718940
Jason Park <parkjs814@gmail.com>
2016-06-03 14:30:36 +0900

    simplify data of bfs/shortest_path

--- algorithm/graph_search/bfs/shortest_path/data.js
+++ algorithm/graph_search/bfs/shortest_path/data.js
@@ -1,12 +1,5 @@
 var tracer = new WeightedDirectedGraphTracer();
 var logger = new LogTracer();
 tracer.attach(logger);
-/*var G = [ // G[i][j] indicates the weight of the path from the i-th node to the j-th node
- [0, 3, 0, 1, 0],
- [5, 0, 1, 2, 4],
- [1, 0, 0, 2, 0],
- [0, 2, 0, 0, 1],
- [0, 1, 3, 0, 0]
- ];*/
-var G = WeightedDirectedGraph.random(10, .3, 1, 9);
+var G = WeightedDirectedGraph.random(5, 1, 1, 9);
 tracer._setData(G);
