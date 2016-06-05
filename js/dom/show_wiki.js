fd6b49df20ea84d371f834764787bad5094d5a54
Jason Park <parkjs814@gmail.com>
2016-06-06 03:51:47 +0900

    fix link within wiki

--- js/dom/show_wiki.js
+++ js/dom/show_wiki.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const app = require('../app');
 const Server = require('../server');
 const converter = new showdown.Converter({tables: true});
 
@@ -7,5 +8,14 @@ module.exports = (wiki) => {
   Server.loadWiki(wiki).then((data) => {
     console.log(data);
     $('#tab_doc > .wrapper').html(converter.makeHtml(data));
+    $('#tab_doc > .wrapper a').click(function (e) {
+      const href = $(this).attr('href');
+
+      console.log(href);
+      if (app.hasWiki(href)) {
+        e.preventDefault();
+        module.exports(href);
+      }
+    });
   });
 };
