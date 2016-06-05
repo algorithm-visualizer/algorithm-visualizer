369067aefb33f4e9b8e25dd8f78245991472f738
Jason Park <parkjs814@gmail.com>
2016-06-06 03:56:44 +0900

    show heading of current wiki

--- js/dom/show_wiki.js
+++ js/dom/show_wiki.js
@@ -6,12 +6,9 @@ const converter = new showdown.Converter({tables: true});
 
 module.exports = (wiki) => {
   Server.loadWiki(wiki).then((data) => {
-    console.log(data);
-    $('#tab_doc > .wrapper').html(converter.makeHtml(data));
+    $('#tab_doc > .wrapper').html(converter.makeHtml(`#${wiki}\n${data}`));
     $('#tab_doc > .wrapper a').click(function (e) {
       const href = $(this).attr('href');
-
-      console.log(href);
       if (app.hasWiki(href)) {
         e.preventDefault();
         module.exports(href);
