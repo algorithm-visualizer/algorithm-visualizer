7684555d4c11ac45c2e6274aa5810dcaee417d4c
Jason Park <parkjs814@gmail.com>
2016-06-10 00:22:38 +0900

    add ChartTracer to most of sorting algorithms

--- algorithm/sorting/merge/top_down_list/data.js
+++ algorithm/sorting/merge/top_down_list/data.js
@@ -1,4 +1,5 @@
-var tracer = new Array1DTracer();
+var chart = new ChartTracer();
+var tracer = new Array1DTracer().attach(chart);
 var logger = new LogTracer();
 var D = Array1D.random(15);
 tracer._setData(D);
