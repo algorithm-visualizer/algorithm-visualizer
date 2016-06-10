f5c50dff05fedfa9f4236f6388ff500d381a65fc
sukyumKim <ikks06luck@naver.com>
2016-06-11 04:51:39 +0900

    modify html&css(scrollbar on IE & FF)

--- js/dom/show_wiki.js
+++ js/dom/show_wiki.js
@@ -6,9 +6,9 @@ const converter = new showdown.Converter({tables: true});
 
 module.exports = (wiki) => {
   Server.loadWiki(wiki).then((data) => {
-    $('#tab_doc > .wrapper').html(converter.makeHtml(`#${wiki}\n${data}`));
+    $('#tab_doc > .tab_inner > .wrapper').html(converter.makeHtml(`#${wiki}\n${data}`));
     $('#tab_doc').scrollTop(0);
-    $('#tab_doc > .wrapper a').click(function (e) {
+    $('#tab_doc > .tab_inner > .wrapper a').click(function (e) {
       const href = $(this).attr('href');
       if (app.hasWiki(href)) {
         e.preventDefault();
