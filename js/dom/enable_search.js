9b008d6edb62b512275437cb8f2a2bd32551ad4f
duaraghav8@gmail <duaraghav8@gmail.com>
2016-06-12 01:13:43 +0530

    Completed Search Feature

--- js/dom/enable_search.js
+++ js/dom/enable_search.js
@@ -4,9 +4,10 @@ module.exports = () => {
 	let $buttons = $('[data-category]');
 
 	$('#search-bar').keyup (function () {
-		let re = new RegExp ($(this).val (), 'i');
+		let query = $(this).val ();
+		let re = new RegExp (query, 'i');
 
-		if ($(this).val ()) $('#footer').hide ();
+		if (query) $('#footer').hide ();
 		else $('#footer').show ();
 
 		$.each ($('#list .category'), function (i, c) {
@@ -18,15 +19,17 @@ module.exports = () => {
 			let cName = $(this).attr ('data-category');
 
 			if ($(this).hasClass ('category')) {
-				//return !re.test ($('[data-category="${cName}"]').text ());
-				return !re.test ($('[data-category="' + cName + '"]').text ());
+				return !re.test ($(`[data-category="${cName}"]`).text ());
 			}
 			else {
 				return !(
-					//re.test ($('.category[data-category="${cName}"]').text()) || re.test ($(this).text ())
-					re.test ($('.category[data-category="' + cName + '"]').text()) || re.test ($(this).text ())
+					re.test ($(`.category[data-category="${cName}"]`).text()) || re.test ($(this).text ())
 				);
 			}
+		}).hide ();
+
+		$('.algorithms').show ().filter (function () {
+			return !$(this).children (':visible').length;
 		}).hide ();
 	});
 };
