0ff9d86a01c554b2e9d6063ee218c8ad81f07995
Kevin Nadro <sodapopcuffgames@gmail.com>
2016-07-04 12:48:46 -0700

    can't spam create the generate overlay

--- js/create/index.js
+++ js/create/index.js
@@ -26,37 +26,40 @@ const saveModules = () => {
 
 const init = () => {
     var moduleWrappers = saveModules();
+    var check = $('.sandbox_container');
+    if(!check.length){
+        util.clearModules();
 
-    util.clearModules();
-    $('.workspace').append("<div class='sandbox_container'>\
-            <section class='close_bar'>\
-            <div class='btn' id='btn_close'>\
-                <div class='wrapper'>\
-                    <i class='fa fa-times' aria-hidden='true'></i>\
+        $('.workspace').append("<div class='sandbox_container'>\
+                <section class='close_bar'>\
+                <div class='btn' id='btn_close'>\
+                    <div class='wrapper'>\
+                        <i class='fa fa-times' aria-hidden='true'></i>\
+                    </div>\
                 </div>\
-            </div>\
-            </section>\
-            <section class='auto-gen auto-gen-tracers'>\
-                <button class='sb-button' id='button-2DMatrix'>Create 2DMatrix</button>\
-            </section>\
-            <section class='auto-gen auto-gen-options'>\
-                <div>\
-                # of Rows: <input class='inputs'id='numRows' type='number' value='5'>\
-                </div>\
-                <div>\
-                # of Columns: <input class='inputs'id='numColumns' type='number' value='5'>\
-                </div>\
-                <div>\
-                Tracer Name: <input class='inputs'id='tracerName' type='text' value='default'>\
-                </div>\
-            </section>\
-            <section class='auto-gen auto-gen-generate'>\
-                <button class='sb-button' id='button-generateJS'>Generate Javascript</button>\
-            </section>\
-    </div>");
-    array2d.setup();
+                </section>\
+                <section class='auto-gen auto-gen-tracers'>\
+                    <button class='sb-button' id='button-2DMatrix'>Create 2DMatrix</button>\
+                </section>\
+                <section class='auto-gen auto-gen-options'>\
+                    <div>\
+                    # of Rows: <input class='inputs'id='numRows' type='number' value='5'>\
+                    </div>\
+                    <div>\
+                    # of Columns: <input class='inputs'id='numColumns' type='number' value='5'>\
+                    </div>\
+                    <div>\
+                    Tracer Name: <input class='inputs'id='tracerName' type='text' value='default'>\
+                    </div>\
+                </section>\
+                <section class='auto-gen auto-gen-generate'>\
+                    <button class='sb-button' id='button-generateJS'>Generate Javascript</button>\
+                </section>\
+        </div>");
+        array2d.setup();
 
-    closeCreate(moduleWrappers);
+        closeCreate(moduleWrappers);
+    }
 };
 
 module.exports = {
