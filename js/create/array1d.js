f7bcdda87b54da1a8c9a09d7d6e548591a460b94
Kevin Nadro <sodapopcuffgames@gmail.com>
2016-07-04 18:41:28 -0700

    working version..
    everything should be fine

---
+++ js/create/array1d.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const array2D = require('./array2d');
+const modules = require('../module');
+const util = require('./util');
+
+const getTracerName = () =>{
+    return document.getElementById("tracerName-1D").value;
+}
+
+const getNumColumns = () => {
+    var column_field = document.getElementById('numColumns-1D');
+    return column_field.value;
+};
+
+const setup = () => {
+    var button_1DMatrix = document.getElementById("button-1DMatrix");
+    var logger;
+    var arr1DTracer;
+    button_1DMatrix.addEventListener('click',function(){
+        util.clearModules();
+        arr1DTracer = new modules.Array1DTracer();
+        var arrElem = document.querySelector('.module_wrapper');
+        arrElem.addEventListener("mousewheel", array2D.mousescroll, false);
+        arrElem.addEventListener("DOMMouseScroll", array2D.mousescroll, false);
+        logger = new modules.LogTracer('Generated Javascript');
+
+        var numColumns = getNumColumns();
+        var data = array2D.fauxData(1,numColumns)[0];
+        console.log(data);
+        arr1DTracer.setData(data);
+        array2D.tableToInputFields(1, numColumns);
+        util.positionModules();
+        arr1DTracer.refresh();
+    },false);
+    var button_JS = document.getElementById('button-generateJS-1D');
+    button_JS.addEventListener('click',function(){
+        array2D.generateJS(logger, 'Array1DTracer');
+    },false);
+};
+
+
+module.exports = {
+    setup
+};
--- js/create/index.js
+++ js/create/index.js
@@ -2,6 +2,7 @@
 
 const modules = require('../module');
 const array2d = require('./array2d');
+const array1d = require('./array1d');
 const util = require('./util');
 const Server = require('../server');
 const DOM = require('../dom');
@@ -32,43 +33,58 @@ const reloadAlgorithm = () => {
     });
 };
 
-
+const createHTML = () => {
+    $('.workspace').append("<div class='sandbox_container'>\
+            <section class='close_bar'>\
+            <div class='btn' id='btn_close'>\
+                <div class='wrapper'>\
+                    <i class='fa fa-times' aria-hidden='true'></i>\
+                </div>\
+            </div>\
+            </section>\
+            <section class='auto-gen'>\
+                <div class='grid' id='array1D-gen'>\
+                <div> array1DTracer </div>\
+                <i class='fa fa-ellipsis-h fa-5x' aria-hidden='true'></i>\
+                <div class='fields'>\
+                # of Columns: <input class='inputs'id='numColumns-1D' type='number' value='5'>\
+                </div>\
+                <div class='fields'>\
+                Tracer Name: <input class='inputs'id='tracerName-1D' type='text' value='default'>\
+                </div>\
+                <button class='sb-button' id='button-1DMatrix'>Create 1DMatrix</button>\
+                <button class='sb-button' id='button-generateJS-1D'>Generate Javascript</button>\
+                </div>\
+                <div class='grid' id='array2D-gen'>\
+                <div> array2DTracer </div>\
+                <i class='fa fa-th fa-5x' aria-hidden='true'></i>\
+                <div class='fields'>\
+                # of Rows: <input class='inputs'id='numRows-2D' type='number' value='5'>\
+                </div>\
+                <div class='fields'>\
+                # of Columns: <input class='inputs'id='numColumns-2D' type='number' value='5'>\
+                </div>\
+                <div class='fields'>\
+                Tracer Name: <input class='inputs'id='tracerName-2D' type='text' value='default'>\
+                </div>\
+                <button class='sb-button' id='button-2DMatrix'>Create 2DMatrix</button>\
+                <button class='sb-button' id='button-generateJS-2D'>Generate Javascript</button>\
+                </div>\
+                <div class='grid' id='chart-gen'></div>\
+                <div class='grid' id='graph-gen'></div>\
+            </section>\
+    </div>");
+};
 
 const init = () => {
 
     var check = $('.sandbox_container');
     if(!check.length){
         util.clearModules();
-
-        $('.workspace').append("<div class='sandbox_container'>\
-                <section class='close_bar'>\
-                <div class='btn' id='btn_close'>\
-                    <div class='wrapper'>\
-                        <i class='fa fa-times' aria-hidden='true'></i>\
-                    </div>\
-                </div>\
-                </section>\
-                <section class='auto-gen auto-gen-tracers'>\
-                    <button class='sb-button' id='button-2DMatrix'>Create 2DMatrix</button>\
-                </section>\
-                <section class='auto-gen auto-gen-options'>\
-                    <div>\
-                    # of Rows: <input class='inputs'id='numRows' type='number' value='5'>\
-                    </div>\
-                    <div>\
-                    # of Columns: <input class='inputs'id='numColumns' type='number' value='5'>\
-                    </div>\
-                    <div>\
-                    Tracer Name: <input class='inputs'id='tracerName' type='text' value='default'>\
-                    </div>\
-                </section>\
-                <section class='auto-gen auto-gen-generate'>\
-                    <button class='sb-button' id='button-generateJS'>Generate Javascript</button>\
-                </section>\
-        </div>");
+        createHTML();
         array2d.setup();
-
-        closeCreate(moduleWrappers);
+        array1d.setup();
+        closeCreate();
     }
 };
 
