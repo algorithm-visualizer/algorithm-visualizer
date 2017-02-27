7cf642aecc95ff58fe06226b96d6f3495089bfdb
Rajkumar Sriramulu <koolraj@gmail.com>
2017-02-27 06:40:06 +0530

    added trace to construct elements
    still work pending in construct and tree code

--- algorithm/tree/binary_search_tree/bst_insert/data.js
+++ algorithm/tree/binary_search_tree/bst_insert/data.js
@@ -1,31 +1,7 @@
-var G = [ // G[i][j] indicates whether the path from the i-th node to the j-th node exists or not
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+var G = [];
 
 
-var T = [ // mapping to G as a binary tree , [i][0] indicates left child, [i][1] indicates right child
-        [-1,-1],
-        [-1,-1],
-        [-1,-1],
-        [-1,-1],
-        [-1,-1],
-        [-1,-1],
-        [-1,-1],
-        [-1,-1],
-        [-1,-1],
-        [-1,-1],
-        [-1,-1]
-];
+var T = {};
 
 var elements = [5,8,10,3,1,6,9,7,2,0,4]; // item to be searched
 var tracer = new DirectedGraphTracer( " BST - Elements marked red indicates the current status of tree ");
