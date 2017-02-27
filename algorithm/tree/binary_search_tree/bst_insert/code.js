274ec63129117b8bd25481f3653650fcd171db9b
Rajkumar Sriramulu <koolraj@gmail.com>
2017-02-27 14:51:22 +0530

    Removed node collection dependecy from consumed app
    udpated code to store nodeCollection and root with in the construct
    tracer

--- algorithm/tree/binary_search_tree/bst_insert/code.js
+++ algorithm/tree/binary_search_tree/bst_insert/code.js
@@ -1,34 +1,32 @@
-function bst_insert ( root, element, parent ) { // node = current node , parent = previous node
+function bst_insert ( root, element, parent ) { // root = current node , parent = previous node
     tracer._visit ( root, parent )._wait ();
+    var treeNode = T[root];
+    var propName = '';    
     if ( element < root ) {
-        if ( T[root][0] === -1 ) {  // insert as left child of root
-            T[root][0] = element;
-            G[root][element] = 1; // update in graph
-            tracer._visit ( element, root )._wait ();
+        propName = 'left';
+    } else if ( element > root) {
+        propName = 'right';
+    }
+    if(propName !== '') {
+        if ( !treeNode.hasOwnProperty(propName) ) {  // insert as left child of root
+            treeNode[propName] = element;
+            T[element] = {};
+            tracer._addNode(element, root);
             logger._print( element + ' Inserted ');
         } else {
-            //tracer._clear ();
-            bst_insert ( T[root][0], element, root );
-        }
-    } else if ( element > root ) { // insert as right child of root
-        if( T[root][1] === -1 ) {
-            T[root][1] = element;
-            G[root][element] = 1; // update in graph
-            tracer._visit ( element, root )._wait ();
-            logger._print ( element + ' Inserted ');
-        } else {
-            //tracer._clear ();
-            bst_insert ( T[root][1], element, root );
+            bst_insert ( treeNode[propName], element, root );
         }
     }
 }
 
 var Root = elements[0]; // take first element as root
+T[Root] = {};
+tracer._addRoot(Root);
 logger._print ( Root + ' Inserted as root of tree ');
-tracer._setTreeData ( G, Root );
 
 for (var i = 1; i < elements.length; i++) {
     tracer2._select ( i )._wait();
     bst_insert ( Root, elements[i] ); // insert ith element
     tracer2._deselect( i );
+    tracer.clear();
 }
--- algorithm/tree/binary_search_tree/bst_insert/data.js
+++ algorithm/tree/binary_search_tree/bst_insert/data.js
@@ -1,10 +1,7 @@
-var G = [];
-
-
 var T = {};
 
 var elements = [5,8,10,3,1,6,9,7,2,0,4]; // item to be searched
-var tracer = new DirectedGraphTracer( " BST - Elements marked red indicates the current status of tree ");
+var tracer = new DirectedGraphConstructTracer( " BST - Elements marked red indicates the current status of tree ", 0);
 var tracer2 = new Array1DTracer ( " Elements ")._setData ( elements );
 var logger = new LogTracer ( " Log ");
 tracer.attach ( logger );
