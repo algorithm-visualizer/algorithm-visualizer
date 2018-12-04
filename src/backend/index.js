4d3df8656ddd396294289c36a42f03077d17f8c5
Jason Park <parkjs814@gmail.com>
2018-12-03 23:37:33 -0500

    Move from heroku to aws ec2 (Close #229)

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
@@ -5,6 +6,7 @@ import bodyParser from 'body-parser';
 import * as controllers from '/controllers';
 import { ClientError, ForbiddenError, NotFoundError, UnauthorizedError } from '/common/error';
 import webhook from '/common/webhook';
+import { execute } from '/common/util';
 
 const app = express();
 app.use(morgan('tiny'));
@@ -26,6 +28,21 @@ app.use((err, req, res, next) => {
   res.status(status);
   res.send(err.message);
   console.error(err);
+});
+
+const rootPath = path.resolve(__dirname, '..', '..');
+webhook.on('algorithm-visualizer', event => {
+  switch (event) {
+    case 'push':
+      execute([
+        'git fetch',
+        'git reset --hard origin/master',
+        'npm install',
+        'npm run build',
+        'pm2 startOrRestart ./pm2.config.js',
+      ].join(' && '), rootPath);
+      break;
+  }
 });
 
 export default app;
