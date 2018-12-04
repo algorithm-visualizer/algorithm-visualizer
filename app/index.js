29d7c719b47368bceb73a37df0085816bb71c2fa
Jason Park <parkjs814@gmail.com>
2018-12-04 14:50:43 -0500

    Redirect old URL to new URL

--- app/index.js
+++ app/index.js
@@ -4,11 +4,15 @@ const express = require('express');
 const app = express();
 
 const {
-  apiEndpoint
+  apiEndpoint,
 } = require('../environment');
 
 const frontend = require('./frontend');
 const backend = require('./backend');
+app.use((req, res, next) => {
+  if (req.hostname === 'algo-visualizer.jasonpark.me') return res.redirect(301, 'http://algorithm-visualizer.org/');
+  next();
+});
 app.use(apiEndpoint, backend);
 app.use(history());
 app.use(compression());
