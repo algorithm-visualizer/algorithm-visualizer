c5f58e895fad15dd6edf0ad550f74b4bc664607f
Jason Park <parkjs814@gmail.com>
2019-02-05 00:15:52 -0500

    Show indices on array tracers

--- src/frontend/core/renderers/Array2DRenderer/index.jsx
+++ src/frontend/core/renderers/Array2DRenderer/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Renderer } from '/core/renderers';
+import { Array1DRenderer, Renderer } from '/core/renderers';
 import styles from './stylesheet.scss';
 import { classes } from '/common/util';
 
@@ -14,13 +14,35 @@ class Array2DRenderer extends Renderer {
   renderData() {
     const { data } = this.props.data;
 
+    const isArray1D = this instanceof Array1DRenderer;
+    let longestRow = data.reduce((longestRow, row) => longestRow.length < row.length ? row : longestRow, []);
+
     return (
       <table className={styles.array_2d}
              style={{ marginLeft: -this.centerX * 2, marginTop: -this.centerY * 2, fontSize: this.zoom }}>
         <tbody>
+        <tr className={styles.row}>
+          {
+            !isArray1D &&
+            <td className={classes(styles.col, styles.index)} />
+          }
+          {
+            longestRow.map((_, i) => (
+              <td className={classes(styles.col, styles.index)}>
+                <span className={styles.value}>{i}</span>
+              </td>
+            ))
+          }
+        </tr>
         {
           data.map((row, i) => (
             <tr className={styles.row} key={i}>
+              {
+                !isArray1D &&
+                <td className={classes(styles.col, styles.index)}>
+                  <span className={styles.value}>{i}</span>
+                </td>
+              }
               {
                 row.map((col, j) => (
                   <td className={classes(styles.col, col.selected && styles.selected, col.patched && styles.patched)}
