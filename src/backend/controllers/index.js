b14bcd3b21a8deef85cd23a592e29bd98abfbbe4
Jason Park <parkjs814@gmail.com>
2019-06-07 15:45:38 +0900

    Separate backend from algorithm-visualizer repo

--- src/backend/controllers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as auth } from './auth';
-export { default as algorithms } from './algorithms';
-export { default as tracers } from './tracers';
-export { default as visualizations } from './visualizations';
--- src/backend/models/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Algorithm } from './Algorithm';
-export { default as Category } from './Category';
-export { default as File } from './File';
-export { default as Hierarchy } from './Hierarchy';
